95a84efdb9d8290e873921e61687959fcef0316f
Alexis C. Gridel <contact@agdl.dev>
2023-08-01 13:14:13 +0200

    feat(infrastructure/tests): initial commit first service

---
+++ tests/DockerSweeper.Infrastrcture.Tests/OperatingSystemServiceTests.cs
@@ -0,0 +1,49 @@
+using DockSweeper.Application.Interfaces;
+using Moq;
+
+namespace DockerSweeper.Infrastrcture.Tests;
+
+public class OperatingSystemServiceTests
+{
+    [Fact]
+    public void IsWindows_ReturnsTrue_WhenRunningOnWindows()
+    {
+        // Arrange
+        var mockOperatingSystemService = new Mock<IOperatingSystemService>();
+        mockOperatingSystemService.Setup(o => o.IsWindows).Returns(true);
+
+        // Act
+        var result = mockOperatingSystemService.Object.IsWindows;
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsLinux_ReturnsTrue_WhenRunningOnLinux()
+    {
+        // Arrange
+        var mockOperatingSystemService = new Mock<IOperatingSystemService>();
+        mockOperatingSystemService.Setup(o => o.IsLinux).Returns(true);
+
+        // Act
+        var result = mockOperatingSystemService.Object.IsLinux;
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsMacOs_ReturnsTrue_WhenRunningOnMacOs()
+    {
+        // Arrange
+        var mockOperatingSystemService = new Mock<IOperatingSystemService>();
+        mockOperatingSystemService.Setup(o => o.IsMacOs).Returns(true);
+
+        // Act
+        var result = mockOperatingSystemService.Object.IsMacOs;
+
+        // Assert
+        Assert.True(result);
+    }
+}
