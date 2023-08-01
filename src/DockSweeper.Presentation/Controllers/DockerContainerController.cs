d0640d6bd3420e38ab76ff03ecbc5fcb740dee93
Alexis C. Gridel <contact@agdl.dev>
2023-08-01 14:45:14 +0200

    feat(controller/dockerContainer): initial commit

---
+++ src/DockSweeper.Presentation/Controllers/DockerContainerController.cs
@@ -0,0 +1,45 @@
+﻿using Docker.DotNet.Models;
+using DockSweeper.Application.Abstractions.Docker;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DockSweeper.Presentation.Controllers;
+
+[ApiController]
+[Route("/api/v1/[controller]")]
+public class DockerContainerController
+    : ControllerBase
+{
+    private readonly IDockerClientFactory _dockerClientFactory;
+    private readonly ILogger<DockerContainerController> _logger;
+
+    public DockerContainerController(
+        ILogger<DockerContainerController> logger,
+        IDockerClientFactory dockerClientFactory)
+    {
+        _logger = logger
+                  ?? throw new ArgumentNullException(nameof(logger));
+        _dockerClientFactory = dockerClientFactory
+                               ?? throw new ArgumentNullException(nameof(dockerClientFactory));
+    }
+
+    [HttpGet(Name = "GetDockerContainers")]
+    public async Task<ActionResult<IEnumerable<ContainerListResponse>>> Get([FromQuery] int limit = 10)
+    {
+        try
+        {
+            var dockerClient = _dockerClientFactory.GetDockerClient();
+            var containers = await dockerClient.Containers.ListContainersAsync(
+                new ContainersListParameters
+                {
+                    Limit = limit
+                });
+
+            return Ok(containers);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Failed to get docker containers");
+            return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+        }
+    }
+}
