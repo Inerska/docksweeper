65a5ec0cb972e5d790d5a764ae0446e0e331a97e
Alexis C. Gridel <contact@agdl.dev>
2023-08-03 14:48:15 +0200

    feat(webclient): add pagination to containers list

--- src/DockSweeper.Presentation/webclient/app/containers/page.tsx
+++ src/DockSweeper.Presentation/webclient/app/containers/page.tsx
@@ -2,7 +2,7 @@
 import {DataTable} from "./data-table"
 
 async function getData(): Promise<Container[]> {
-    const response = await fetch("http://localhost:5236/api/v1/DockerContainer?all=false");
+    const response = await fetch("http://localhost:5236/api/v1/DockerContainer?all=true");
     return await response.json();
 }
 
