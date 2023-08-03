bef27beabbe85d3eae94e208f03f2b2bfa78cabb
Alexis C. Gridel <contact@agdl.dev>
2023-08-03 17:05:46 +0200

    style(detail/container): hover and link arrow to go back

--- src/DockSweeper.Presentation/webclient/app/docker/containers/[id]/page.tsx
+++ src/DockSweeper.Presentation/webclient/app/docker/containers/[id]/page.tsx
@@ -1,5 +1,32 @@
-﻿export default function Page({params}: { params: { id: string } }) {
-    return (<div>
-        <h1>Page {params.id}</h1>
+﻿import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import {ArrowLeft, ContainerIcon} from "lucide-react";
+import Link from "next/link";
+
+export default function Page({params}: { params: { id: string } }) {
+    return (
+        <div>
+        <header className="mb-5 flex flex-col gap-3">
+            <Link href={`/`} className="hover:-translate-x-5 transition-transform duration-200 mb-5 ease-in-out hover:text-slate-500">
+                <ArrowLeft/>
+            </Link>
+            <h1 className="font-semibold text-3xl">Container details</h1>
+            <h2 className="text-slate-500">{params.id}</h2>
+        </header>
+        <Card>
+            <CardHeader className="flex flex-row w-full items-center gap-5">
+                <ContainerIcon/>
+                <CardTitle className="text-2xl">Container Information</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <ul className="flex flex-col gap-2">
+                    <li>Id: {params.id}</li>
+                    <li>Names: {params.id}</li>
+                    <li>Images: {params.id}</li>
+                    <li>Command: {params.id}</li>
+                    <li>Created: {params.id}</li>
+                    <li>State: {params.id}</li>
+                </ul>
+            </CardContent>
+        </Card>
     </div>);
 }
