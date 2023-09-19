d8f8237e892443f47febb80f52fe929dd53509c4
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-09-19 10:54:47 +0200

    feat: to bun, and set up dev env again

--- src/DockSweeper.Presentation/webclient/app/containers/page.tsx
+++ src/DockSweeper.Presentation/webclient/app/containers/page.tsx
@@ -1,13 +1,11 @@
-﻿export const revalidate = 0;
-
-import {columns, Container} from "./columns"
+﻿import {columns, Container} from "./columns"
 import {DataTable} from "./data-table"
 
 async function getData(): Promise<Container[]> {
     let data = [];
 
     try {
-        const response = await fetch(`http://api/api/v1/DockerContainer?all=true`);
+        const response = await fetch(`http://localhost:5236/api/v1/DockerContainer?all=true`);
         data = await response.json();
     } catch (err) {
         console.error('Failed to fetch data:', err);
